9974037ba216cfb2e71164bf6a0bf16fce665ea3
Rebecca Sharkey <rebeccajaynesharkey@gmail.com>
2024-04-18 16:03:05 +0100

    Added Project

---
+++ LearningGAS/Source/LearningGAS/LearningGAS.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LearningGAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearningGAS, "LearningGAS" );
+ 
